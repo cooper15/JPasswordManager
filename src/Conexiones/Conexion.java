d2b99bb1770b4b64a3c7c0697f32065d7ea42353
cooper15 <cooper15@localhost>
2014-08-08 18:44:46 -0600

    Agregado método para llamar procedimiento almacenado.

--- src/Conexiones/Conexion.java
+++ src/Conexiones/Conexion.java
@@ -3,7 +3,9 @@ package Conexiones;
 
 import com.mysql.jdbc.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -11,13 +13,29 @@ import java.sql.SQLException;
  */
 public class Conexion {
     
-    protected void conectar(){
+    private java.sql.Connection conectar(){
+        java.sql.Connection conexion;
         try {
              Class.forName("com.mysql.jdbc.Driver");
-             java.sql.Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/Password", "root", "antares15"); 
+             conexion = DriverManager.getConnection("jdbc:mysql://localhost/Password", "root", "antares15"); 
+             return conexion;
         }
         catch (SQLException | ClassNotFoundException exception){
+            return null;
         }
-  
+
+    }
+    
+    protected void insertarUsuario( String datos){
+        java.sql.Connection conexion= conectar();
+        try {
+            if (conexion != null){
+             Statement estado = conexion.createStatement(); 
+              estado.executeQuery ("Call agregarUsuario("+datos+")");
+            }
+           
+        }
+        catch(SQLException e){}
+        
     }
 }
